9086fa229cd8ed55fa848749b5a731ef31d884a7
Dilshan <dilshanramesh81@gamil.com>
2019-04-14 21:59:01 +0530

    Updates

---
+++ precache-manifest.0a8347b4e05f7ce7f66de4741eda667e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38677e4c8e3c2a0008ad",
+    "url": "/Cryptocurrency-Ticker/static/js/runtime~main.de77640f.js"
+  },
+  {
+    "revision": "25ae7b3c88597daaf4a3",
+    "url": "/Cryptocurrency-Ticker/static/js/main.16b453ca.chunk.js"
+  },
+  {
+    "revision": "979821f6756ba7dc378a",
+    "url": "/Cryptocurrency-Ticker/static/js/2.7569e5d4.chunk.js"
+  },
+  {
+    "revision": "25ae7b3c88597daaf4a3",
+    "url": "/Cryptocurrency-Ticker/static/css/main.1171de78.chunk.css"
+  },
+  {
+    "revision": "661bf903f756d432ff445e17d2db4b23",
+    "url": "/Cryptocurrency-Ticker/index.html"
+  }
+];
